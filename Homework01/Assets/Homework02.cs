b4cf78b6566f9a529a5772b702c996daa23711d9
khim1569 <khim1569@gmail.com>
2015-09-10 03:15:06 +0700

    HomeWork02 Coroutine เอาไปใช้ในการกดใช้สกิลแล้วติดcooldown

---
+++ Homework01/Assets/Homework02.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Homework02 : MonoBehaviour {
+	public Button Putskill;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void notUpdate () {
+		StartCoroutine (Skill ());
+	
+	}
+
+	IEnumerator Skill(){
+		Putskill.interactable = false;
+		yield return new WaitForSeconds (3f);
+		Putskill.interactable = true;
+	}
+}
